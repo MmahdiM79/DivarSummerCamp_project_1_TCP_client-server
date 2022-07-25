a33b8a343aff82f6a6c5d76e5d633e2413f354a6
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2022-07-25 12:54:13 +0430

    client.py file completed

--- client/client.py
+++ client/client.py
@@ -1,5 +1,9 @@
-from utils.functions import clear
+import sys
+sys.path.insert(1, '/Users/mm.m.mm/Desktop/divar project 1/')
 
+from utils.functions import clear
+from sys import orig_argv
+from socket import AF_INET, SOCK_STREAM, socket
 
 
 
@@ -9,3 +13,26 @@ from utils.functions import clear
 
 if __name__ == '__main__':
     clear()
+    
+    # removing the first and second arguments. (the script name and the path)
+    orig_argv.pop(0)
+    orig_argv.pop(0)
+    
+    
+    HOST = orig_argv[0]
+    PORT = int(orig_argv[1])
+    
+    
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((HOST, PORT))
+    
+    print(f'connected to server > host:{HOST}, port:{PORT}')
+    
+    s.sendall(input('please enter your key: ').encode())
+    s.sendall(input('please enter your data: ').encode())
+    
+    response = s.recv(1024).decode()
+    print(f'\n\n\nresponse: {response}\n')
+    
+    s.close()
+    
