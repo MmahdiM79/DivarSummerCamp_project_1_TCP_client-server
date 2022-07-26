caaef75afe492307c103d0b278a4ddf5b0b3d929
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2022-07-26 09:10:19 +0430

    client class added

--- client/client.py
+++ client/client.py
@@ -8,6 +8,66 @@ from socket import AF_INET, SOCK_STREAM, socket
 
 
 
+class TcpClient(object):
+    
+    def __init__(self, host: str='127.0.0.1', port: int=8080, buffer_size: int=1024):
+        '''
+            :param host: hostname or ip address. (default: 127.0.0.1)
+            :param port: port number. (default: 8080)
+            :param buffer_size: size of the buffer. (default: 1024)
+        '''
+        self.host = host
+        self.port = port
+        self.buffer_size = buffer_size
+        self.__sock = socket(AF_INET, SOCK_STREAM)
+        
+    
+
+    def connect(self) -> None:
+        '''
+            connect to server.
+        '''
+        self.__sock.connect((self.host, self.port))
+        
+        
+        
+    def send(self, data: str) -> int:
+        '''
+            send data to server.\n
+            the data will be encoded to utf-8.
+            
+            :param data: data to send.
+            :return: number of bytes sent.
+        '''
+        return self.__sock.send(data.encode())
+    
+    
+    
+    def recv(self) -> str:
+        '''
+            receive data from server with buffer_size.\n
+            the data will be decoded from utf-8.
+            
+            :return: data received.
+        '''
+        return self.__sock.recv(self.buffer_size).decode()
+    
+    
+    
+    def __str__(self) -> str:
+        return f'host: {self.host}\nport: {self.port}\nbuffer_size: {self.buffer_size}'
+    
+    def __del__(self) -> None:
+        self.__sock.close()
+        
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def __eq__(self, __o: object) -> bool:
+        return self.__str__() == __o.__str__()
+
+
+
 
 
 
